62c59ba43adb9aa9a334eda368195c8883714ec4
kthth0707 <kimtaehyun00728@gmail.com>
2022-07-18 22:28:27 +0900

    완성

--- TodoListProject/ViewController.swift
+++ TodoListProject/ViewController.swift
@@ -10,23 +10,68 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var tableViewTodo: UITableView!
+    @IBOutlet var viewTodo: UIView!
+    @IBOutlet var buttonEdit: UIBarButtonItem!
     
-    var tasks = [Task]()
+    var buttonDone: UIBarButtonItem?
+    var tasks = [Task]() {
+        didSet {
+            self.saveTasks()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.loadTasks()
         self.initSetting()
     }
     
     func initSetting() {
         self.tableViewTodo.dataSource = self
+        self.tableViewTodo.delegate = self
+        
+        self.buttonDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneClick))
+        
+        if self.tasks.isEmpty {
+            self.viewTodo.isHidden = false
+        } else {
+            self.viewTodo.isHidden = true
+        }
+        
     }
     
+    func saveTasks() {
+        let data = self.tasks.map {
+            [
+                "title" : $0.title,
+                "done" : $0.done
+            ]
+        }
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(data, forKey: "tasks")
+    }
+    
+    func loadTasks() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "tasks") as? [[String:Any]] else { return }
+        self.tasks = data.compactMap {
+            guard let title = $0["title"] as? String else { return nil }
+            guard let done = $0["done"] as? Bool else { return nil }
+            return Task(title: title, done: done)
+        }
+    }
+    
+    @objc func doneClick() {
+        self.navigationItem.leftBarButtonItem = self.buttonEdit
+        self.tableViewTodo.setEditing(false, animated: true)
+    }
     
 
     @IBAction func onClickEdit(_ sender: UIBarButtonItem) {
-        
+        guard !self.tasks.isEmpty else { return }
+        self.navigationItem.leftBarButtonItem = self.buttonDone
+        self.tableViewTodo.setEditing(true, animated: true)
     }
     
     @IBAction func onClickAdd(_ sender: Any) {
@@ -35,6 +80,7 @@ class ViewController: UIViewController {
         guard let title = alert.textFields?[0].text else { return }
         let task = Task(title: title, done: false)
         self?.tasks.append(task)
+        self?.viewTodo.isHidden = true
         self?.tableViewTodo.reloadData()
         }
         let cancelButton = UIAlertAction(title: "취소", style: .default, handler: nil)
@@ -61,12 +107,54 @@ extension ViewController: UITableViewDataSource {
     }
 }
 
+extension ViewController: UITableViewDelegate {
+    
+    //정렬 기능을 사용하기 위해선
+    //두개의 함수를 실행 해줘야 함
+    //canMoveRowAt,moveRowAt
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        var tasks = self.tasks
+        let task = tasks[sourceIndexPath.row]
+        tasks.remove(at: sourceIndexPath.row)
+        tasks.insert(task, at: destinationIndexPath.row)
+        self.tasks = tasks
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        self.tasks.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        
+        if self.tasks.isEmpty {
+            self.doneClick()
+            self.viewTodo.isHidden = false
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var task = self.tasks[indexPath.row]
+        task.done = !task.done
+        self.tasks[indexPath.row] = task
+        self.tableViewTodo.reloadRows(at: [indexPath], with: .automatic)
+    }
+}
+
 class TodoListCell: UITableViewCell {
     
     @IBOutlet var labelTitle: UILabel!
     
     func initSetting(item: Task) {
         self.labelTitle.text = item.title
+        
+        if item.done {
+            self.accessoryType = .checkmark
+        } else {
+            self.accessoryType = .none
+        }
     }
 }
 
